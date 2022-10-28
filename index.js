c51b35369c49e57de4b4dabc22267c64caa0e64c
Dani Lipari <lipari.assistenza@gmail.com>
2022-10-28 18:17:24 +0200

    updates

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import os from "os";
-import morgan from "morgan";
-import { config } from "dotenv";
-config();
+const express = require('express');
+const cors = require("cors");
+const helmet = require('helmet');
+require('dotenv').config();
+const os = require('os');
+const morgan = require('morgan');
+const vision = require('@google-cloud/vision');
 
 /*
   https://medium.com/analytics-vidhya/setting-up-google-cloud-vision-api-with-node-js-db29d1b6fbe2
@@ -19,12 +19,33 @@ config();
   app.use(express.urlencoded({extended: true}));
   app.use(express.json());
   app.use(morgan('combined'));
-  const PORT = process.env.PORT || 8081;
   app.use(express.json());
 
   app.use(cors());
 
-  /* Homepage Backend */
+  async function quickstart() {
+    const client = new vision.ImageAnnotatorClient({
+      keyFilename: "./vision-366914-97afb4cde767.json",
+    });
+
+    /* const [result] = await client.labelDetection('./images/poker.jpeg');
+    const labels = result.labelDetection;
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description)); */
+
+    /* const image = './images/poker.jpeg'; */
+    const image = './images/carte-gioco.jpeg';
+    /* const image = './images/test.png'; */
+    let [result] = await client.textDetection(image);
+    console.log('----------------------');
+    console.log('Text:', result.textAnnotations[0]);
+    console.log('----------------------');
+    console.log('Description:', result.textAnnotations[0].description);
+  }
+
+  quickstart();
+
+
   app.get('/', (req, res) =>
     res.send(`<h2">Image Recognition Google Cloud Vision API System</h2>`)
   );
